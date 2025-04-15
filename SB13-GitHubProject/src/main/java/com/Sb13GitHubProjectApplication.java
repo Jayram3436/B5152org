3c936a823bbd70d202c09bcbb018a4bd42146857
Jayram3436 <jayramdhande63@gmail.com>
2025-04-16 02:01:03 +0530

    project added

---
+++ SB13-GitHubProject/src/main/java/com/Sb13GitHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb13GitHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb13GitHubProjectApplication.class, args);
+	}
+
+}
